f93dc2e6fe8e858dbc2e7d8635481b1049708744
Higor Zenaide <higorz.almeida@gmail.com>
2025-08-05 08:35:22 -0300

    Atualizações para o google

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yvbombdbcdyappuziwgx.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2Ym9tYmRiY2R5YXBwdXppd2d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNjM1OTcsImV4cCI6MjA2OTczOTU5N30.ecTlDkcXE0GAYYSfaz_oci0QqqH1vmIKbue1TBUV8n8'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
