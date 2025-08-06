a8b9ec7eb1542e93b0030c844d7f1378f733488a
Higor Zenaide <higorz.almeida@gmail.com>
2025-08-06 17:04:25 -0300

    Remove unused imports

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'screens/home_screen.dart';
-import 'services/notification_service.dart';
-import 'services/whatsapp_service.dart';
 import 'test_screen.dart';
 
 void main() {
